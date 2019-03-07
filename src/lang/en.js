37ae80c0e695456aa66b24c6765f11b00dc44917
liuxingui <ascarecrow@126.com>
2019-03-07 18:28:29 +0800

    。

--- src/lang/en.js
+++ src/lang/en.js
@@ -123,7 +123,9 @@ export default {
     readings: 'Readings',
     status: 'Status',
     actions: 'Actions',
+    detail: 'Detail',
     edit: 'Edit',
+    updatePwd: 'Change Password',
     publish: 'Publish',
     draft: 'Draft',
     delete: 'Delete',
@@ -157,5 +159,226 @@ export default {
     close: 'Close',
     closeOthers: 'Close Others',
     closeAll: 'Close All'
+  },
+  // 社区
+  community: {
+    communityNo: 'Community Number',
+    communityName: 'Community Name',
+    communityAddress: 'Community Address', // 社区全址
+    communityArea: 'Total Area', // 总面积(平方米)
+    communityBuildingArea: 'Construction Area', // 建筑面积
+    communityCommonArea: 'Common Area', // 公用面积
+    communityContacts: 'Contact', // 联系人
+    communityContactsPhone: 'Contact Number', // 联系人号码
+    communityGarageArea: 'Garage Area', // 车库面积
+    communityGarageCount: 'Parking Apace Number', // 车位数量
+    communityGreenarea: 'Green Area', // 绿化面积
+    communityLocX: 'Community Latitude', // 社区纬度
+    communityLocY: 'Community Longitude', // 社区经度
+    communityManagementType: 'Management Type', // 管理类型(0普通管理1综合管理)
+    communityRemark: 'Remarks', // 备注
+    communityRoadArea: 'Road Area', // 道路面积
+    communityRoomCount: 'Rooms Number' // 房间总数
+  },
+  // 管理员
+  administrator: {
+    communityId: 'Community', // 社区id
+    email: 'Email', // 邮箱
+    managerId: 'id',
+    name: 'Name', // 名字
+    sex: 'Gender', // 性别0女1男
+    type: 'Type', // 类型0普通管理员1超级管理员
+    username: 'Username', // 用户名
+    password: 'Password' // 密码
+  },
+  // 建筑
+  building: {
+    buildingDirection: 'Building Direction', // 楼宇方向
+    buildingId: 'ID', // buildingId
+    buildingName: 'Building Name', // 楼宇名字
+    buildingNo: 'Building Number', // 楼宇编号
+    buildingStruct: 'Building Structure', // 楼宇结构
+    communityId: 'Community', // 社区ID
+    deleted: 'Logout', // 是否注销
+    floorLowNum: 'Several Floors Downstairs', // 楼下几层
+    floorUpNum: 'Several Floors Upstairs', // 楼上几层
+    fullAddress: 'Building Address' // 楼宇全址
+  },
+  // 单位
+  unit: {
+    unitChildRelativeProportion: 'Relative Parts Of A Hierarchical Building(Thousandth)', // 分层建筑物之子部分相对比(千分之一)
+    buildingId: 'Building', // buildingId
+    community: 'Community',
+    unitCoveredArea: 'Coverage Area(Unit Square Meter)', // 覆盖面积大小(单位平方米)
+    unitName: 'Unit Name', // 单元名字
+    unitFullAddress: 'Address', // 全址
+    unitId: 'ID', // unitId
+    unitNo: 'Unit No', // 单位编号
+    unitPosition: 'Position', // 位置(地下,一楼,二楼,三楼,户外.)
+    unitPurpose: 'Use', // 用途
+    unitRelativeProportion: 'Stratified Building Relative(Thousandth)', // 分层建筑物相对比(千分之一)
+    unitStatus: 'Unit Status', // 单位状态0空置1租赁2装修中3入住
+    unitType: 'Unit Type' // 单位类型1商铺2住宅3停车场
+  },
+  // 业主
+  proprietor: {
+    birthday: 'Birthday', // 出生日期
+    communityId: 'Community', // 社区ID
+    countryCode: 'Area Aode', // 区号
+    createTime: 'Create Time', // 创建时间
+    email: 'Email', // 邮编
+    englishName: 'English Name', // 英文名字
+    deleted: false, // 是否注销
+    idCard: 'Identification Number', // 身份证号码
+    marriageSystem: 'Marriage System', // 婚姻制度
+    mateName: 'Spouse Name', // 配偶名字
+    name: 'Name', // 名字
+    portrait: 'Profile Picture', // 用户头像
+    sex: 'Gender', // 性别(0女1男)
+    tel: 'Phone Number', // 手机号
+    updateTime: 'Update Time', // 更新时间
+    userId: 'UserId', // 用户id
+    username: 'Login Account', // 用户登录账号
+    password: 'Password' // 密码
+  },
+  asset: {
+    assetDescribe: 'Description', // 描述
+    assetEnglishDescribe: 'Description（English）', // 描述（英文）
+    assetEnglishName: 'Asset Name（English）', // 资产名字（英文）
+    assetEnglishPosition: 'Location Information（English）', // 位置信息（英文）
+    assetId: 'ID', // id
+    assetName: 'Asset Name', // 资产名字
+    assetPosition: 'Location Information', // 位置信息
+    assetTraditionalDescribe: 'Description（Traditional Chinese）', // 描述（繁体）
+    assetTraditionalName: 'Asset Name（Traditional Chinese）', // 资产名字（繁体）
+    assetTraditionalPosition: 'Location Information（Traditional Chinese）', // 位置信息（繁体）
+    assetType: 'Asset Type', // 资产类型：电器、公共设备
+    community: 'Community',
+    communityId: 'Community ID' // 社区ID
+  },
+  notice: {
+    buildingId: 'Building', // 建筑id
+    communityId: 'Community ID', // 社区ID
+    community: 'Community', // 社区ID
+    createTime: 'Create Time', // 创建时间
+    noticeDetails: 'Detail', // 详情
+    noticeEnglishDetails: 'Detail（English）', // 详情英文
+    noticeEnglishTitle: 'Place Title（English）', // 场所标题
+    noticeId: 'ID', // ID
+    noticeImage: 'Image', // 图片
+    noticeTitle: 'Place Title', // 场所标题
+    noticeTraditionalDetails: 'Detail（Traditional Chinese）', // 详情（繁体）
+    noticeTraditionalTitle: 'Title（Traditional Chinese）', // 标题
+    noticeType: 'Notification Type', // 通知类型
+    updateTime: 'Update Time' // 更新时间
+  },
+  event: {
+    community: 'Community',
+    communityId: 'Community ID', // 社区ID
+    eventCause: 'Event Cause', // 事件起因
+    eventContent: 'Event Content', // 事件内容
+    eventDate: 'Event Date', // 事件日期
+    eventFinishDate: 'Complete Time', // 完成时间
+    eventId: 'ID', // ID
+    eventRemark: 'Remark', // 备注
+    eventSolve: 'Solution', // 解决方案
+    eventStatus: 'Event Progress', // 事件进度
+    eventType: 'Event Type' // 事件类型1采购2保养3其他
+  },
+  adv: {
+    adv: 'Advertisement',
+    advId: 'ID', // ID
+    building: 'Building',
+    buildingId: 'Building ID', // 楼宇id
+    community: 'Community', // 社区
+    communityId: 'communityId', // communityId
+    describe: 'Description', // 描述
+    residenceTime: 'Play Time',
+    title: 'Title', // title
+    type: 'Advertisement Type', // 广告类型 0普通图片1视频
+    url: 'Resource', // url
+    used: 'Used' // 是否使用0否1是
+  },
+  complain: {
+    complain: 'Complaint/Warranty',
+    complainClassType: 'Classification', // 类型分类（供电系统，发电机…）
+    complainDescribe: 'Description', // 描述
+    complainFinishTime: 'Complete Time', // 完成时间
+    complainHandler: 'Processing Person', // complainHandler
+    community: 'Community',
+    complainId: 'id', // complainId
+    complainLiaisonsEmail: 'Contact Email',
+    complainLiaisonsName: 'Contact', // complainLiaisonsName
+    complainLiaisonsSex: 'Contact Gender', // 联络人性别(0女1男)
+    complainPosition: 'Position', // complainPosition
+    complainStatus: 'Status', // 状态:0发起1收到2处理中3处理完成
+    complainType: 'Type', // 类型(土木工程，清洁，保安…)
+    complainVoice: 'Voice', // example: E:/XXX/投诉.mp3
+    createTime: 'Create Time', // createTime
+    images: 'Image', // images 【】
+    updateTime: 'Update Time', // updateTime
+    userId: 'UserId',
+    complainSpecificPosition: 'Specific Location' // complainSpecificPosition
+  },
+  visitor: {
+    building: 'Building',
+    buildingId: 'BuildingId', // 楼宇id
+    community: 'Community', // 社区
+    communityId: 'communityId', // communityId
+    createTime: 'Create Time', // createTime
+    updateTime: 'Update Time', // updateTime
+    visitorCause: 'Access Reason', // visitorCause
+    visitorId: 'id', // visitorId
+    visitorName: 'Visitor Name', // visitorName
+    visitorSex: 'Gender', // 性别0女1男
+    visitorPhone: 'Contact Number' // visitorPhone
+  },
+  clubhouse: {
+    community: 'Community',
+    createTime: 'Create Time', // createTime
+    updateTime: 'Update Time', // updateTime
+    images: 'Image', // images 【】
+    placeAdvanceOrderDay: 'Days In Advance', // 提前天数
+    placeEndTime: 'End Time', // placeEndTime
+    placeEnglishIntroduction: 'Introduction', // 简介(英文)
+    placeEnglishName: 'Place Name(English)', // 场所名字(英文)
+    placeFarthestOrderDay: '最大天数', // 最大天数
+    placeId: 'id', // placeId
+    placeIntroduction: 'Introduction(中文)',
+    placeName: 'Place Name(Simplified)', // 场所名字(简体)
+    placeNeedOrder: 'Book', // 需要预定(0否1是)
+    placeStartTime: 'Start Time', // 起始时间
+    placeStatus: 'Location Status', // 场所状态(0未开放1开放)
+    placeTraditionalIntroduction: 'Introduction(Simplified)', // 简介(简体)
+    placeTraditionalName: 'Place Name(Traditional Chinese)', // 场所名字(繁体)
+    placeUpperLimit: 'Appointment Time Limit'
+  },
+  charge: {
+    recordActualAmount: 'Actual Amount Charged', // 实际收取金额
+    recordAmount: 'Charge Amount', // 收费金额
+    recordDate: 'Charge Record Year', // 收费记录年月份(2019-01)
+    recordId: '', // recordId
+    recordLateDate: 'Late Gold Days', // 滞纳金天数
+    recordLateFee: 'Delayed Amount', // 滞纳金额
+    recordRemark: 'Remark', // 备注
+    recordStatus: 'Status', // 状态0欠费1已付2预支付
+    recordTime: 'Charging Time', // 收费时间
+    unitItemId: 'Unit Charge Item', // 单位收费项目
+    userId: 'User' // 住户
+  },
+  chargeItem: {
+    additionalCost: 'Additional Charges(Fund Fee)', // 额外费用(基金费用)
+    alculationMethod: 'Calculation', // 计算方式0定额1公式
+    billingMode: 'Charging Mode', // 收费模式0周期性1临时性
+    communityId: 'Community Id', // 社区id
+    community: 'Community', // 社区
+    describe: 'Amount', // 金额
+    formula: 'Calculation Formula', // 计算公式
+    itemId: 'id', // itemId
+    itemName: 'Charge Item Name', // 收费项目名字
+    itemNo: 'Charge Item No', // 收费项目编号
+    lateFee: 'Late Payment Rate', // 滞纳金率(单位:百分之一)
+    unitPrice: 'Calculate Unit Price', // 计算单价
+    lateDate: 'Days Of Late Start' // 滞纳开始天数
   }
 }

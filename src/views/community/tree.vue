dabb934b609a21f2f951e56a4e23ff2e1448c645
liuxingui <ascarecrow@126.com>
2019-09-11 02:18:57 +0800

    Update tree.vue

--- src/views/community/tree.vue
+++ src/views/community/tree.vue
@@ -1,219 +1,101 @@
 <template>
   <div style="height: 100%;width: 100%;position: absolute;padding: 15px;">
-    <el-tabs type="border-card" style="height: 100%;">
+    <el-tabs type="border-card" style="height: 100%;" @tab-click="tabClick">
       <el-tab-pane v-for="(item, index) in treeData" :key="index" :label="item.communityName">
-        <el-tabs tab-position="left" style="height: 700px;">
-          <el-tab-pane :key="0" :label="item.communityName">
-            <div>显示表格信息？社区信息？</div>
-            <!-- <el-form ref="form" label-width="150px">
+        <el-tabs tab-position="left" style="height: 700px;" @tab-click="buildTabClick">
+          <el-card class="box-card" style="margin-bottom: 15px;">
+            <el-row :gutter="20">
+              <el-col :span="3"><div style="font-size: 20px;font-weight: 800;">社区信息</div></el-col>
+              <el-col v-if="communityInfo" :span="21">
+                <el-row :gutter="20" style="padding-bottom: 15px;">
+                  <el-col :span="6">{{ $t('community.communityName') }}:{{ communityInfo.communityName || '--' }}</el-col>
+                  <el-col :span="6">{{ $t('community.communityArea') }}:{{ communityInfo.communityArea || '--' }}</el-col>
+                  <el-col :span="12">{{ $t('community.communityAddress') }}:{{ communityInfo.communityAddress || '--' }}</el-col>
+                  <!-- <el-col :span="6">{{ $t('community.communityName') }}:{{ communityInfo.communityName || '--' }}</el-col> -->
+                </el-row>
+                <el-row :gutter="20">
+                  <el-col :span="6">{{ $t('community.communityManagementType') }}:{{ communityInfo.communityManagementType === 0 ? '简单管理' : '综合管理' || '--' }}</el-col>
+                  <el-col :span="6">{{ $t('community.communityRoomCount') }}:{{ communityInfo.communityRoomCount || '--' }}</el-col>
+                  <el-col :span="6">{{ $t('community.communityGarageCount') }}:{{ communityInfo.communityGarageCount || '--' }}</el-col>
+                  <el-col :span="6">{{ $t('community.communityShopsCount') }}:{{ communityInfo.communityShopsCount || '--' }}</el-col>
+                </el-row>
+              </el-col>
+            </el-row>
+          </el-card>
+          <el-tab-pane v-for="(buildingInfo, k) in item.buildingWithUnits" :key="k" :label="(buildingInfo.buildingName) + '（建筑）'">
+            <el-card class="box-card" style="margin-bottom: 15px;">
               <el-row :gutter="20">
-                <el-col :span="12">
-                  <el-form-item :label="$t('community.communityName')">
-                    <div>：{{ item.communityName || '--' }}</div>
-                  </el-form-item>
-                </el-col>
-                <el-col :span="12">
-                  <el-form-item :label="$t('community.communityAddress')">
-                    <div>：{{ item.communityAddress || '--' }}</div>
-                  </el-form-item>
+                <el-col :span="3"><div style="font-size: 20px;font-weight: 800;">建筑信息</div></el-col>
+                <el-col v-if="buildingDetail" :span="20">
+                  <el-row :gutter="21">
+                    <el-col :span="6">{{ $t('building.buildingNo') }}:{{ buildingDetail.buildingNo || '--' }}</el-col>
+                    <el-col :span="6">{{ $t('building.buildingName') }}:{{ buildingDetail.buildingName || '--' }}</el-col>
+                    <el-col :span="6">{{ $t('building.communityId') }}:{{ buildingDetail.community.communityName || '--' }}</el-col>
+                    <el-col :span="6">{{ $t('building.floorLowNum') }}:{{ buildingDetail.floorLowNum || '--' }}</el-col>
+                  </el-row>
                 </el-col>
               </el-row>
-              <el-row :gutter="20">
-                <el-col :span="12">
-                  <el-form-item :label="$t('community.communityArea')">
-                    <div>：{{ item.communityArea || '--' }}</div>
-                  </el-form-item>
-                </el-col>
-                <el-col :span="12">
-                  <el-form-item :label="$t('community.communityArea')">
-                    <div>：{{ item.communityArea || '--' }}</div>
-                  </el-form-item>
-                </el-col>
-              </el-row>
-              <el-row :gutter="20">
-                <el-col :span="12">
-                  <el-form-item :label="$t('community.communityManagementType')">
-                    <div>：{{ item.communityManagementType || '--' }}</div>
-                  </el-form-item>
-                </el-col>
-                <el-col :span="12">
-                  <el-form-item :label="$t('community.communityRoomCount')">
-                    <div>：{{ item.communityRoomCount || '--' }}</div>
-                  </el-form-item>
-                </el-col>
-              </el-row>
-              <el-row :gutter="20">
-                <el-col :span="12">
-                  <el-form-item :label="$t('community.communityGarageCount')">
-                    <div>：{{ item.communityGarageCount || '--' }}</div>
-                  </el-form-item>
-                </el-col>
-                <el-col :span="12">
-                  <el-form-item :label="$t('community.communityShopsCount')">
-                    <div>：{{ item.communityShopsCount || '--' }}</div>
-                  </el-form-item>
-                </el-col>
-              </el-row>
-            </el-form> -->
-            <!-- <div style="text-align: center;"><el-button size="mini" type="primary" @click="$router.push('/community/edit/' + item.communityId)">{{ $t('table.detail') }}</el-button></div> -->
-          </el-tab-pane>
-          <el-tab-pane v-for="(unitInfo, k) in item.unitList" :key="k + 1" :label="(unitInfo.unitName) + '（建筑）'">
-            <div>显示表格信息？建筑信息？</div>
-            <!-- <el-form v-if="unitInfo" ref="form" label-width="150px">
-              <el-row :gutter="20">
-                <el-col :span="12">
-                  <el-form-item :label="$t('building.buildingNo')">
-                    <div>：{{ unitInfo.buildingNo || '--' }}</div>
-                  </el-form-item>
-                </el-col>
-                <el-col :span="12">
-                  <el-form-item :label="$t('building.buildingName')">
-                    <div>：{{ unitInfo.buildingName || '--' }}</div>
-                  </el-form-item>
-                </el-col>
-              </el-row>
-              <el-row :gutter="20">
-                <el-col :span="12">
-                  <el-form-item :label="$t('building.communityId')">
-                    <div>：{{ unitInfo.communityName || '--' }}</div>
-                  </el-form-item>
-                </el-col>
-                <el-col :span="12">
-                  <el-form-item :label="$t('building.floorLowNum')">
-                    <div>：{{ unitInfo.floorLowNum || '--' }}</div>
-                  </el-form-item>
-                </el-col>
-              </el-row> -->
-            <!-- </el-form> -->
-            <!-- <div style="text-align: center;"><el-button size="mini" type="primary" @click="$router.push('/community/edit/' + item.communityId)">{{ $t('table.detail') }}</el-button></div> -->
+            </el-card>
+            <div style="font-size: 20px;font-weight: 800;margin-bottom: 15px;">单元信息</div>
+            <el-table :data="buildingInfo.unitList" row-key="label" border fit highlight-current-row style="height: 460px;overflow-y: auto;">
+              <el-table-column :label="$t('unit.unitNo')" prop="id" align="center" min-width="100">
+                <template slot-scope="scope">
+                  <span>{{ scope.row.unitNo }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column :label="$t('unit.unitName')" min-width="180px" align="center">
+                <template slot-scope="scope">
+                  <span>{{ scope.row.unitName }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column :label="$t('unit.buildingId')" min-width="120px" align="center">
+                <template slot-scope="scope">
+                  <span>{{ scope.row.building ? scope.row.building.buildingName : '--' }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column :label="$t('unit.unitCoveredArea')" min-width="110px" align="center">
+                <template slot-scope="scope">
+                  <span>{{ scope.row.unitCoveredArea }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column :label="$t('unit.unitFullAddress')" min-width="180px" align="center">
+                <template slot-scope="scope">
+                  <span>{{ scope.row.unitFullAddress }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column :label="$t('unit.unitPosition')" min-width="80px" align="center">
+                <template slot-scope="scope">
+                  <span>{{ scope.row.unitPosition }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column :label="$t('unit.unitStatus')" min-width="100px" align="center">
+                <template slot-scope="scope">
+                  <span>{{ scope.row.unitStatus | unitStatusFilter }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column :label="$t('unit.unitTitle')" min-width="100px" align="center">
+                <template slot-scope="scope">
+                  <span>{{ scope.row.unitTitle }}</span>
+                </template>
+              </el-table-column>
+            </el-table>
           </el-tab-pane>
         </el-tabs>
       </el-tab-pane>
     </el-tabs>
-    <!-- <el-container style="height: 100%; border-right: 1px solid #eee">
-      <el-aside width="200px" style="border-right: 1px solid #eee">
-        <el-tree :data="treeData" :props="defaultProps" @node-click="handleNodeClick" />
-      </el-aside>
-      <el-container style="padding: 0 15px;">
-        <div style="width: 100%;overflow-y: auto;">
-          <el-table v-if="showType === 1" :data="buildingList" row-key="label" border fit highlight-current-row>
-            <el-table-column :label="$t('building.buildingNo')" prop="id" align="center" min-width="100">
-              <template slot-scope="scope">
-                <span>{{ scope.row.buildingNo }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column :label="$t('building.buildingName')" min-width="180px" align="center">
-              <template slot-scope="scope">
-                <span>{{ scope.row.buildingName }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column :label="$t('building.managementType')" min-width="180px" align="center">
-              <template slot-scope="scope">
-                <span>{{ scope.row.managementType === 0 ? '简单管理' : '综合管理' }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column :label="$t('building.buildingDirection')" min-width="120px" align="center">
-              <template slot-scope="scope">
-                <span>{{ scope.row.buildingDirection }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column :label="$t('building.buildingStruct')" min-width="110px" align="center">
-              <template slot-scope="scope">
-                <span>{{ scope.row.buildingStruct }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column :label="$t('building.communityId')" min-width="180px" align="center">
-              <template slot-scope="scope">
-                <span>{{ scope.row.community.communityName }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column :label="$t('building.floorLowNum')" min-width="80px" align="center">
-              <template slot-scope="scope">
-                <span>{{ scope.row.floorLowNum }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column :label="$t('building.floorUpNum')" min-width="80px" align="center">
-              <template slot-scope="scope">
-                <span>{{ scope.row.floorUpNum }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column :label="$t('building.fullAddress')" min-width="180px" align="center">
-              <template slot-scope="scope">
-                <span>{{ scope.row.fullAddress }}</span>
-              </template>
-            </el-table-column>
-          </el-table>
-          <el-table v-if="showType === 2" :data="unitList" row-key="label" border fit highlight-current-row>
-            <el-table-column :label="$t('unit.unitPurpose')" min-width="150px" align="center">
-              <template slot-scope="scope">
-                <span>{{ scope.row.unitType | unitTypeFilter }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column :label="$t('unit.unitNo')" prop="id" align="center" min-width="100">
-              <template slot-scope="scope">
-                <span>{{ scope.row.unitNo }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column :label="$t('unit.unitName')" min-width="180px" align="center">
-              <template slot-scope="scope">
-                <span>{{ scope.row.unitName }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column :label="$t('unit.buildingId')" min-width="120px" align="center">
-              <template slot-scope="scope">
-                <span>{{ scope.row.building.buildingName }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column :label="$t('unit.unitCoveredArea')" min-width="110px" align="center">
-              <template slot-scope="scope">
-                <span>{{ scope.row.unitCoveredArea }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column :label="$t('unit.unitFullAddress')" min-width="180px" align="center">
-              <template slot-scope="scope">
-                <span>{{ scope.row.unitFullAddress }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column :label="$t('unit.unitPosition')" min-width="80px" align="center">
-              <template slot-scope="scope">
-                <span>{{ scope.row.unitPosition }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column :label="$t('unit.unitStatus')" min-width="100px" align="center">
-              <template slot-scope="scope">
-                <span>{{ scope.row.unitStatus | unitStatusFilter }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column :label="$t('unit.unitTitle')" min-width="100px" align="center">
-              <template slot-scope="scope">
-                <span>{{ scope.row.unitTitle }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column :label="$t('unit.unitRelativeProportion')" width="140px" align="center">
-              <template slot-scope="scope">
-                <span>{{ scope.row.unitRelativeProportion }}</span>
-              </template>
-            </el-table-column>
-            <el-table-column :label="$t('unit.unitChildRelativeProportion')" width="190px" align="center">
-              <template slot-scope="scope">
-                <span>{{ scope.row.unitChildRelativeProportion }}</span>
-              </template>
-            </el-table-column>
-          </el-table>
-        </div>
-      </el-container>
-    </el-container> -->
   </div>
 </template>
 
 <script>
   import {
     getCommunityTree,
-    getBuildingList
+    getBuildingList,
+    getBuildingDetail
   } from '@/api/building'
   import {
     getUnitList
   } from '@/api/unit'
+  import { queryCommunityDetail } from '@/api/community'
   import Pagination from '@/components/Pagination' // Secondary package based on el-pagination
   // import { ElTree } from 'element-ui'
 
@@ -278,13 +160,15 @@
         treeProps: {
           children: 'children',
           hasChildren: 'hasChildren'
-        }
+        },
+        communityInfo: null,
+        buildingDetail: null,
+        communityIndex: 0
       }
     },
     computed: {
       treeData() {
-        console.log('resData', this.resData)
-        return this.resData.map(item => {
+        const data = this.resData.map(item => {
           let children = []
           if (item.buildingWithUnits) {
             children = item.buildingWithUnits.map(value => {
@@ -322,6 +206,8 @@
             children
           }
         })
+        // console.log('data', data)
+        return data
       }
     },
     watch: {},
@@ -330,6 +216,35 @@
       this.queryBuildingList()
     },
     methods: {
+      tabClick(e) {
+        this.communityIndex = e.index
+        this.getCommunityDetail(this.treeData[e.index].communityId)
+        const buildingId = this.treeData[e.index].buildingWithUnits[0] ? this.treeData[e.index].buildingWithUnits[0].buildingId : ''
+        this.querBuildingDetail(buildingId)
+      },
+      buildTabClick(e) {
+        const buildingId = this.treeData[this.communityIndex].buildingWithUnits[e.index] ? this.treeData[this.communityIndex].buildingWithUnits[e.index].buildingId : ''
+        this.querBuildingDetail(buildingId)
+      },
+      // 获取社区详情
+      async getCommunityDetail(communityId) {
+        const { code, msg, data } = await queryCommunityDetail({ communityId }).catch(e => e)
+        if (code !== 200) {
+          return this.$notify({ title: '失败', message: msg, type: 'error', duration: 2000 })
+        }
+        this.communityInfo = { ...data }
+      },
+      // 获取建筑详情
+      async querBuildingDetail(buildingId) {
+        this.buildingDetail = null
+        if (!buildingId) return
+        const { code, msg, data } = await getBuildingDetail({ buildingId }).catch(e => e)
+        if (code !== 200) {
+          return this.$notify({ title: '失败', message: msg, type: 'error', duration: 2000 })
+        }
+        // console.log('res.data', data)
+        this.buildingDetail = { ...data }
+      },
       async getList() {
         this.listLoading = true
         const response = await getCommunityTree().catch(e => e)
@@ -345,6 +260,9 @@
         this.list = this.resData[0].buildingWithUnits || this.resData[0].unitList || []
         this.key = this.resData[0].communityId || this.resData[0].buildingId
         this.communityId = this.resData[0].communityId
+        // 第一次加载的是第一个社区信息
+        this.getCommunityDetail(this.treeData[0].communityId)
+        this.querBuildingDetail(this.treeData[0].buildingWithUnits[0].buildingId)
         this.listLoading = false
       },
       handleNodeClick(data) {
